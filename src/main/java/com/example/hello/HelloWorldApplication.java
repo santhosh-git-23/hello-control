2b5e3e6314235e5c9217a5e06fb95ab501b8f8f6
santhosh-git-23 <129048391+santhosh-git-23@users.noreply.github.com>
2023-06-02 12:21:21 +0530

    Add files via upload

---
+++ src/main/java/com/example/hello/HelloWorldApplication.java
@@ -0,0 +1,14 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.example")
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
